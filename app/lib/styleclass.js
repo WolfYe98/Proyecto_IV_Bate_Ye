9081c89c1d146b7d95efc1bc7f39ffd79df59688
Bate Ye <mingye@hotmail.es>
2020-10-05 18:19:06 +0200

    Actualizado lib

--- app/lib/styleclass.js
+++
@@ -1,9 +0,0 @@
-
-class DanceStyle{
-  constructor(estilo, historia, cuerpo){
-    this.estilo = estilo;
-    this.historia = historia;
-    this.cuerpo = cuerpo;
-  }
-}
-module.exports = DanceStyle;
