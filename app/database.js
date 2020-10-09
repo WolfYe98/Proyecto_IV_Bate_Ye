b78312aeb3ba586ba8e3f549eb379e4751c0bab7
WolfYe98 <mingye@hotmail.es>
2020-10-10 00:29:01 +0200

    Añadido constructor y funcion getStyles

--- app/database.js
+++ app/database.js
@@ -1,6 +1,12 @@
 var fs=require('fs');
 
 class Database{
+  /**
+  * @function constructor
+  * @summary Constructor de la clase Database, recibe un parámetro path y extrae los datos de un fichero json
+  * @author Bate Ye
+  * @param {string} path Ruta al archivo que guardan los datos
+  */
   constructor(path){
     this.path = path;
     try{
@@ -13,9 +19,22 @@ class Database{
     }
     console.log(this.data);
   }
+
+    /**
+    * @function getStyles
+    * @summary Método para consultar los estilos de baile.
+    * @author Bate Ye
+    * @returns {array} Devuelve un array con los nombres de los diferentes estilos de baile.
+    */
   getStyles(){
     return Object.keys(this.data);
   }
+    /**
+    * @function close
+    * @summary Método para cerrar la "conexión" a la base de datos
+    * @author Bate Ye
+    * 
+    */
   close(){
     this.path = undefined;
     this.dataFile = undefined;
