9f4b8b7654bf30139bb924cc9f22b86cf11cdf6d
WolfYe98 <mingye@hotmail.es>
2020-10-10 00:35:00 +0200

    Tarea default creada

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
-
+gulp.task('default', gulp.series('test'));
 gulp.task('test', async function(){
   gulp.src('test')
   .pipe(mocha());
