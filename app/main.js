f9fa224e8897fad930fd64d5ab0030564102f07f
WolfYe98 <mingye@hotmail.es>
2020-10-09 19:00:36 +0200

    Actualizado main.js

--- app/main.js
+++ app/main.js
@@ -1,6 +1,9 @@
 var http =require("http");
+var Database = require("./database.js");
+var db = new Database(__dirname+"/data/data.json");
+
 
 http.createServer(function(req,res){
-  res.write("hola mundo!");
+  console.log(db.data);
   res.end();
 }).listen(8080);
