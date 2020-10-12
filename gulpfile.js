cdb36586c767f97d9561de17520a504cd08672e8
WolfYe98 <mingye@hotmail.es>
2020-10-12 18:40:29 +0200

    Añadido tests para la clase database. #6

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var run= require('gulp-run');
 
+
+//Tarea para testear el proyecto.
 gulp.task('test', async function(){
   gulp.src('test')
   .pipe(mocha());
+});
+
+//Tarea para instalar las dependencias: aun que con npm install se puede hacer más fácil
+gulp.task('install', async function(){
+  gulp.src('./')
+    .pipe(run('npm install'))
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('default', gulp.series('test'));
