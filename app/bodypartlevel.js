e14e78ae430f27463c6910c98d3c6e23f137d8f0
WolfYe98 <mingye@hotmail.es>
2020-11-02 19:08:12 +0100

    Documentada la clase bodyPartLevel #19

--- app/bodypartlevel.js
+++ app/bodypartlevel.js
@@ -1,5 +1,11 @@
 
 class BodyPartLevel{
+  /**
+  * @function Constructor
+  * @summary Constructor que recibe el bodyPart y el estilo del baile
+  * @author Bate Ye
+  * @param {string} style Es el nombre del estilo de baile que queremos saber sus usos del cuerpo.
+  */
   constructor(bodyPart, style){
     this.levels = {
       "hiphop":{
@@ -18,22 +24,48 @@ class BodyPartLevel{
       this.useLevel = -1;
     }
   }
+  /**
+  * @function getBodyPart
+  * @summary Método que devuelve un string que es el nombre de la parte del cuerpo
+  * @author Bate Ye
+  * @returns {string} Devuelve el nombre de la parte del cuerpo
+  */
   getBodyPart(){
     return this.bodyPart;
   }
+  /**
+  * @function getUseLevel
+  * @summary Método que devuelve el nivel de uso que se le da a esta parte del cuerpo en este estilo
+  * @author Bate Ye
+  * @returns {number} Devuelve el nombre de la parte del cuerpo
+  */
   getUseLevel(){
     return this.useLevel;
   }
+  /**
+  * @function setBodyPart
+  * @summary Método que cambia la parte del cuerpo del objeto
+  * @param {string} body Es el nuevo nombre del bodyPart
+  * @author Bate Ye
+  */
   setBodyPart(body){
     if(typeof body === "string"){
       this.bodyPart = body;
       this.useLevel = this.levels[this.style][this.bodyPart];
     }
   }
+  /**
+  * @function setUseLevel
+  * @summary Método que cambia el nivel de uso de la parte del cuerpo
+  * @param {number} level Es el nuevo nivel de uso
+  * @author Bate Ye
+  */
   setUseLevel(level){
     if(typeof level === "number"){
-      this.levels[this.bodyPart] = level;
-      this.useLevel = level;
+      if(level >= 0 && level <= 3){
+        this.levels[this.bodyPart] = level;
+        this.useLevel = level;
+      }
     }
   }
 }
