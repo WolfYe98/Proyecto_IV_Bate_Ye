3ac7e894ec5e79425d38251ae416becec908e6b1
WolfYe98 <mingye@hotmail.es>
2020-11-02 18:57:56 +0100

    Algunas pruebas

--- app/main.js
+++ app/main.js
@@ -5,5 +5,6 @@ var db = new Database(__dirname+"/data/data.json");
 
 http.createServer(function(req,res){
   res.write(JSON.stringify(db.data,null,4));
+  db.getBodyPartArray("hiphop");
   res.end();
 }).listen(3000);
