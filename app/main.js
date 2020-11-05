e216940a803bc8e0544c1ac54736e2035ec26c90
WolfYe98 <mingye@hotmail.es>
2020-11-05 23:24:14 +0100

    Pruebas básicas

--- app/main.js
+++ app/main.js
@@ -1,10 +1,12 @@
 var http =require("http");
 var Database = require("./database.js");
 var db = new Database(__dirname+"/data/data.json");
-
+var BodyPartLevel = require('./bodypartlevel.js')
+var recommendation = require('./recommendation.js');
 
 http.createServer(function(req,res){
   res.write(JSON.stringify(db.data,null,4));
-  db.getBodyPartArray("hiphop");
+  var n = new BodyPartLevel('piernas', 'hiphop');
+  recommendation.recommendation([new BodyPartLevel('cadera', 2),new BodyPartLevel('pecho', 0)]);
   res.end();
 }).listen(3000);
