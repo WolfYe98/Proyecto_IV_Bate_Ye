eb5e011cdfa60cf12b75fac4dda75620f0a4ff03
WolfYe98 <mingye@hotmail.es>
2020-11-22 23:49:21 +0100

    Pruebas con funciones creadas para usar el micro-api

--- app/main.js
+++ app/main.js
@@ -4,9 +4,10 @@ var db = new Database(__dirname+"/data/data.json");
 var BodyPartLevel = require('./bodypartlevel.js')
 var recommendation = require('./recommendation.js');
 var fetch = require('node-fetch');
-
+var consultarPrecioGeneral = require('./prices.js').consultarPrecioGeneral;
 http.createServer(async function(req,res){
-  var url = "https://academies-pricing.vercel.app/api/academiesPricing";
-  var data = await fetch(url).then(resp=>resp.json()).then(data=>{return data});
+  //var url = "https://academies-pricing.vercel.app/api/academiesPricing";
+  //var data = await fetch(url).then(resp=>resp.json()).then(data=>{return data});
+  var data = await consultarPrecioGeneral();
   res.end(JSON.stringify(data));
 }).listen(3000);
