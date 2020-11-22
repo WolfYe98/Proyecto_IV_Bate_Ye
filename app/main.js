2e974e7afefb768f350b197a580994347a04c936
WolfYe98 <mingye@hotmail.es>
2020-11-22 23:14:06 +0100

    Probando async/await y el micro-api

--- app/main.js
+++ app/main.js
@@ -3,10 +3,10 @@ var Database = require("./database.js");
 var db = new Database(__dirname+"/data/data.json");
 var BodyPartLevel = require('./bodypartlevel.js')
 var recommendation = require('./recommendation.js');
+var fetch = require('node-fetch');
 
-http.createServer(function(req,res){
-  res.write(JSON.stringify(db.data,null,4));
-  var n = new BodyPartLevel('piernas', 'hiphop');
-  recommendation.recommendation([new BodyPartLevel('cadera', 2),new BodyPartLevel('pecho', 0)]);
-  res.end();
+http.createServer(async function(req,res){
+  var url = "https://academies-pricing.vercel.app/api/academiesPricing";
+  var data = await fetch(url).then(resp=>resp.json()).then(data=>{return data});
+  res.end(JSON.stringify(data));
 }).listen(3000);
