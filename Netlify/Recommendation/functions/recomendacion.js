fdf5ac79b8d2a08ccf4ea1e6f763e7ea996aeefe
WolfYe98 <mingye@hotmail.es>
2020-11-24 13:28:17 +0100

    Creado y añadido función serverless netlify en el proyecto

---
+++ Netlify/Recommendation/functions/recomendacion.js
@@ -0,0 +1,60 @@
+const levels = {
+  hiphop:{
+      cadera:3,
+      piernas:2,
+      pecho: 1,
+      brazos: 1
+  },
+  krump:{
+      brazos:3,
+      piernas:3,
+      pecho: 1,
+      cadera: 1
+  }
+}
+exports.handler = async function(event, context) {
+  var partes;
+  if(event.queryStringParameters != {}){
+    partes = event.queryStringParameters;
+  }
+  else{
+    partes = 'nothing';
+  }
+  if(typeof partes ==  'object'){
+    var keysPartes = Object.keys(partes);
+    var keyStyle = Object.keys(levels);
+    var recomendados={};
+    for(const s in levels){
+      recomendados[s] = 0;
+    }
+    for(var i = 0; i < keysPartes.length; i++){
+      for(var j = 0; j< keyStyle.length; j++){
+        if(levels[keyStyle[j]][keysPartes[i]] <= parseInt(partes[keysPartes[i]])){
+          recomendados[keyStyle[j]] += 1;
+        }
+      }
+    }
+    var max = 0;
+    for(var x = 0; x < keyStyle.length; x++){
+      if(recomendados[keyStyle[x]] > max){
+        max = recomendados[keyStyle[x]];
+      }
+    }
+    var retorno={};
+    for(z = 0; z < keyStyle.length; z++){
+      if(recomendados[keyStyle[z]] == max){
+        retorno[z+1] = keyStyle[z];
+      }
+    }
+    return{
+      statusCode: 200,
+      body: JSON.stringify({recommendedStyle: retorno})
+    };
+  }
+  else{
+    return {
+      statusCode: 200,
+      body: JSON.stringify({warningMessage:'This api is for recommend a dancing style, you have to give some bodyparts in spanish and their intensity'})
+    };
+  }
+}
