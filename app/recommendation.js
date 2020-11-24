6d6655bd965f54df038f81852223a1069aa3df0d
WolfYe98 <mingye@hotmail.es>
2020-11-24 17:19:41 +0100

    Modificado funcion recommendation.js

--- app/recommendation.js
+++ app/recommendation.js
@@ -1,48 +1,25 @@
 var BodyPartLevel = require('./bodypartlevel.js');
+var fetch = require('node-fetch');
 /**
 * @function recommendation
 * @summary Función que devuelve cual sería el estilo más recomendable según las partes del cuerpo que les han pasado
 * @author Bate Ye
 * @param {Array} bodyParts Es un array de objetos de BodyPartLevel, indican qué partes del cuerpo y que nivel de uso suele tener el usuario de esas partes.
 */
-function recommendation(bodyParts){
-  var bp = new BodyPartLevel('cadera', 10);
-  var bpsIntensity = bp.getStyleBodyPartIntensity();
-  var styles = Object.keys(bpsIntensity);
-  var recommendStyles =[];
-
-  styles.forEach(function (element, index, array){
-    var obj = {};
-    obj[element] = 0;
-    recommendStyles.push(obj);
-  });
-  if(typeof bodyParts === 'object'){
-    bodyParts.forEach(function(element,index,array){
-      styles.forEach(function(e,i,a){
-        if(bpsIntensity[e] != undefined){
-          if(element.getBodyPart() in bpsIntensity[e]){
-            if(element.getUseLevel() >= bpsIntensity[e][element.getBodyPart()]){
-              var i;
-              for(i = 0; i < recommendStyles.length; i++){
-                if(Object.keys(recommendStyles[i]) == e){
-                  recommendStyles[i][e] = recommendStyles[i][e]+1;
-                }
-              }
-            }
-          }
-        }
-      });
-    });
+async function recommendation(bodyParts){
+  var URL = 'https://dancinform-recomendation.netlify.app/.netlify/functions/recomendacion';
+  var i = 0;
+  URL = URL+'?'bodyParts[0].getBodyPart()+'='+bodyParts[0].getUseLevel().toString();
+  for(i = 1; i < bodyParts.length; i++){
+    URL += '&'+bodyParts[i].getBodyPart()+'='+bodyParts[i].getUseLevel().toString();
   }
-  var max = recommendStyles[0];
-  var k = Object.keys(recommendStyles[0]);
-  for(var i = 1; i < recommendStyles.length; i++){
-    var s = Object.keys(recommendStyles[i]);
-    if(max[k] < recommendStyles[i][s]){
-      max = recommendStyles[i];
-    }
+  var obj = await fetch(URL).then(res=>res.json()).then(datos=>{return datos}).catch(err=>console.log(err));
+  var keysObject = Object.keys(obj);
+  var array = [];
+  for(i = 0; i < keysObject.length;i++){
+    array.push(keysObject[i]);
   }
-  return Object.keys(max)[0].toUpperCase();
+  return array;
 }
 
 
