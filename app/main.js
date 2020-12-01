50c4aabeb70eced6f8df13422ca447ec34b996f0
WolfYe98 <mingye@hotmail.es>
2020-12-01 20:26:20 +0100

    Añadido ruta para consultar estilos #28

--- app/main.js
+++ app/main.js
@@ -1,14 +1,44 @@
-var http =require("http");
-var Database = require("./database.js");
-var db = new Database(__dirname+"/data/data.json");
-var BodyPartLevel = require('./bodypartlevel.js')
+const fastify = require('fastify');
+const Database = require("./database.js");
+var db = new Database(undefined);
+var Styles = require('./styles.js');
+var styles = new Styles(db);
 var recommendation = require('./recommendation.js');
-var fetch = require('node-fetch');
 var consultarPrecioGeneral = require('./prices.js').consultarPrecioGeneral;
 var consultarPrecioCiudad = require('./prices.js').consultarPrecioCiudad;
-http.createServer(async function(req,res){
-  //var url = "https://academies-pricing.vercel.app/api/academiesPricing";
-  //var data = await fetch(url).then(resp=>resp.json()).then(data=>{return data});
-  var data = await consultarPrecioCiudad('los angeles');
-  res.end(JSON.stringify(data));
-}).listen(3000);
+
+var app = fastify();
+
+//ruta para consultar todos los estilos.
+app.get('/allstyles',(req,res)=>{
+  res.send({
+    styles:styles.getStyles()
+  });
+});
+
+//ruta para obtener un estilo en concreto y todas sus informaciones.
+app.get('/style/:styleName',(req,res)=>{
+  try{
+    var st = styles.getStyleByName(req.params.styleName);
+    res.code(200);
+    res.send({style:st});
+  }catch(err){
+    console.log(err);
+    res.code(404);
+    res.send({
+      statusCode: res.statusCode,
+      message:'Not found'
+    });
+  }
+});
+
+//Ruta para obtener estilos en cada ciudad.
+
+app.listen(3000,(err)=>{
+  if(err){
+    console.log(err);
+    throw err;
+    process.exit(1);
+  }
+  console.log('Success!');
+});
