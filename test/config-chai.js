73284346fd849be5ed2a68c8d2f309c3ff0c2af7
WolfYe98 <mingye@hotmail.es>
2020-12-10 22:02:29 +0100

    Añadido tests a la lectura de la configuración externa

---
+++ test/config-chai.js
@@ -0,0 +1,15 @@
+const expect = require('chai').expect;
+const exteportConfig = require('../app/config.js');
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+
+describe('Testing external configuration function',()=>{
+  it('Should return an Object',async()=>{
+    var config =await exteportConfig();
+    expect(config).to.be.an('object');
+  });
+  it('Should return an int PORT',async()=>{
+    var config =await exteportConfig();
+    expect(config.port).to.be.above(0);
+  });
+});
