5233cc312c48891a61f96500b5d0ba3334223063
WolfYe98 <mingye@hotmail.es>
2020-12-10 21:42:57 +0100

    Añado configuración #30

--- app/server.js
+++ app/server.js
@@ -1,16 +1,18 @@
-var build = require('./main.js');
-
-var app = build({
-  logger:{
-    level:'error',
-    prettyPrint:true
-  }
-});
-app.listen(3000,(err)=>{
-  if(err){
-    console.log(err);
-    throw err;
-    process.exit(1);
-  }
-  console.log('Success!');
-});
+const build = require('./main.js');
+const exportConfig = require('./config.js');
+(async()=>{
+  var configs = await exportConfig();
+  var app = build({
+    logger:{
+      prettyPrint:true
+    }
+  });
+  app.listen(parseInt(configs.port),(err)=>{
+    if(err){
+      console.log(err);
+      throw err;
+      process.exit(1);
+    }
+    console.log(`Success!`);
+  });
+})();
